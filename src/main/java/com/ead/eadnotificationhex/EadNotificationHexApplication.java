acfa28fc8ba907c4759096a89c33d6376f1a0546
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-06-18 12:59:39 -0300

    project setup

---
+++ src/main/java/com/ead/eadnotificationhex/EadNotificationHexApplication.java
@@ -0,0 +1,13 @@
+package com.ead.eadnotificationhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadNotificationHexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadNotificationHexApplication.class, args);
+    }
+
+}
